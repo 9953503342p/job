1fbcbc000d8ef499fb47ddac9e169b9064531aca
9953503342p <168403027+9953503342p@users.noreply.github.com>
2025-01-14 11:27:47 +0530

    Update server.js

--- jobzila-node/server.js
+++ jobzila-node/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const bcrypt = require('bcrypt js');
+const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
